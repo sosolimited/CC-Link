044df9b7dcd55c4d37d03888b2fd7f0f1214182f
laurasher <asherlk1@mac.com>
2016-04-22 13:48:53 -0700

    Added more control codes to map.

--- src/link/ClosedCaptionConversions.hpp
+++ src/link/ClosedCaptionConversions.hpp
@@ -154,10 +154,20 @@ const auto special_closed_caption_to_string = ([]() {
 
 const auto control_code_to_string = ([]() {
     auto mapping = std::unordered_map<char, std::string>{
-        { 0x2D, u8" " }, // Carriage Return
-        { 0x21, u8" " }, // Tab
+        { 0x20, u8" " }, // Resume Caption Loading
+        { 0x21, u8" " }, // Backspace
         { 0x22, u8" " }, // Tab
-        { 0x23, u8" " } // Tab
+        { 0x23, u8" " }, // Tab
+				{ 0x24, u8" " }, // Delete to End of Row
+				{ 0x25, u8" " }, // Roll-Up Captions–2 Rows
+				{ 0x26, u8" " }, // Roll-Up Captions–3 Rows
+				{ 0x27, u8" " }, // Roll-Up Captions–4 Rows
+				{ 0x28, u8" " }, // Flash On
+				{ 0x29, u8" " }, // Resume Direct Captioning
+				{ 0x2A, u8" " }, // Text Restart
+				{ 0x2B, u8" " }, // Resume Text Display
+				{ 0x2C, u8" " }, // Erase Displayed Memory
+				{ 0x2D, u8" " }  // Carriage Return
     };
 
     return [mapping](char cc_character) -> std::string {
